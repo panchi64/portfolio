bad4d06df9f3e2a645e96607d5cc73ce1b974364
Francisco Casiano <{ID}+{username}@users.noreply.github.com>
2021-10-27 17:13:48 -0400

    Finished layout tutorial

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,4 +1,14 @@
-import { VStack, Heading, Text, Button } from "@chakra-ui/react";
+import {
+  VStack,
+  Heading,
+  Text,
+  Button,
+  HStack,
+  AspectRatio,
+  Image,
+  Stack,
+  Divider,
+} from "@chakra-ui/react";
 
 const Cart = () => {
   return (
@@ -20,6 +30,48 @@ const Cart = () => {
           </Button>
         </Text>
       </VStack>
+      <HStack spacing={6} alignItems="center" w="full">
+        <AspectRatio ratio={1} w={24}>
+          <Image
+            src="https://pbs.twimg.com/media/E2U5d9pXMAQz8n-?format=jpg&name=large"
+            alt="Computer burning"
+          />
+        </AspectRatio>
+        <Stack
+          spacing={0}
+          w="full"
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <VStack w="full" spacing={0} alignItems="flex-start">
+            <Heading size="md">Penny board</Heading>
+            <Text color="gray">PNYCOMP27541</Text>
+          </VStack>
+          <Heading size="sm" textAlign="end">
+            $119.00
+          </Heading>
+        </Stack>
+      </HStack>
+      <VStack spacing={4} alignItems="stretch" w="full">
+        <HStack justifyContent="space-between">
+          <Text color="gray">Subtotal</Text>
+          <Heading size="sm">$119.00</Heading>
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text color="gray">Shipping</Text>
+          <Heading size="sm">$19.99</Heading>
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text color="gray">Taxes (estimated)</Text>
+          <Heading size="sm">$23.80</Heading>
+        </HStack>
+      </VStack>
+      <Divider />
+      <HStack justifyContent="space-between" w="full">
+        <Text color="gray">Total</Text>
+        <Heading size="lg">$162.79</Heading>
+      </HStack>
     </VStack>
   );
 };
